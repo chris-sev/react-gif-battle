f371417e041b24227365458f17da2175136fad65
Chris Sevilleja <chris@scotch.io>
2018-08-16 17:24:42 -0500

    reddy freddy

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,25 +2,27 @@ import React from 'react';
 
 const Header = ({ isAuthenticated, login, logout }) => (
   <nav className="navbar is-dark">
-    {/* logo */}
-    <div className="navbar-brand">
-      <a className="navbar-item router-link-exact-active router-link-active">
-        GIPHY BATTLE 🙈🙉🙊🐵
-      </a>
-    </div>
+    <div className="container">
+      {/* logo */}
+      <div className="navbar-brand">
+        <a className="navbar-item router-link-exact-active router-link-active">
+          GIPHY BATTLE 🙈🙉🙊🐵
+        </a>
+      </div>
 
-    {/* links */}
-    <div className="navbar-menu">
-      <div className="navbar-end">
-        {isAuthenticated ? (
-          <a className="navbar-item" onClick={logout}>
-            Logout
-          </a>
-        ) : (
-          <a className="navbar-item" onClick={login}>
-            Login
-          </a>
-        )}
+      {/* links */}
+      <div className="navbar-menu is-active">
+        <div className="navbar-end">
+          {isAuthenticated ? (
+            <a className="navbar-item" onClick={logout}>
+              Logout
+            </a>
+          ) : (
+            <a className="navbar-item" onClick={login}>
+              Login
+            </a>
+          )}
+        </div>
       </div>
     </div>
   </nav>
